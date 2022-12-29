51251b7e124b47d2762a69b21b533858be64bbfa
Someone <120342697+Somespi@users.noreply.github.com>
2022-12-29 20:18:11 +0400

    Create getClient.py

---
+++ hino/getClient.py
@@ -0,0 +1,9 @@
+def getclient(info: str) -> str:
+            """Return client info about Hino"""
+	    from hinoapi import api
+            info = info.lower().replace(" ", "")
+            for i in list(client.keys())[:-2]:
+                if i in info:
+                    return str(client[i])
+            else:
+                raise TypeError(f"\"{info}\" is not defined in \"client\".")
